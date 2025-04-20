4678aef76010eae4a414d8619205a3ee63703751
Yechiel Babani <yechielb2000@gmail.com>
2025-04-21 00:44:02 +0300

    add template function

--- cnc/cnc-core/routers/agent.py
+++ cnc/cnc-core/routers/agent.py
@@ -8,10 +8,11 @@ provisioner_router = APIRouter(
 
 
 @provisioner_router.get(path="install")
-async def install_agent(version: Optional[str] = None):
+async def install_agent(password: str, version: Optional[str] = None):
     """
     Get latest version of agent. If version is provided, it will install the specified version.
     If the specified version is not available, it will return None.
+    To install anything you must provide a password. The password is unique per each agent, configured by user.
     """
     pass
 
@@ -21,3 +22,11 @@ async def register_agent():
     """Register new agent, add new agent record."""
     pass
 
+
+@provisioner_router.get(path="challenge")
+async def challenge_agent():
+    """
+    As an agent you must answer a challenge before making any other action.
+    After succeeding this challenge it can send data and receive its new tasks.
+    """
+    pass
