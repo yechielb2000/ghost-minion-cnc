2c3609296263301a0e006fc80f8c46664d74663d
Yechiel Babani <yechielb2000@gmail.com>
2025-05-18 20:00:08 +0300

    add TaskStatus to __all__

--- shared/models/__init__.py
+++ shared/models/__init__.py
@@ -1,9 +1,10 @@
 from shared.models.agent import Agent
 from shared.models.data import Data
-from shared.models.task import Task
+from shared.models.task import Task, TaskStatus
 
 __all__ = [
     'Agent',
     'Data',
-    'Task'
+    'Task',
+    'TaskStatus',
 ]
