a4df0f458bd090f59dc0e2851e11fadc9e856ec2
Yechiel Babani <yechielb2000@gmail.com>
2025-07-09 19:21:32 +0300

    add gateway

---
+++ gateway/config.py
@@ -0,0 +1,10 @@
+# TODO: use config file (or even better, find a good way to sync with ks3)
+
+TASKS_PROVIDERS = [
+    "http://tasks_provider1:8000",
+    "http://tasks_provider2:8000",
+]
+DATA_RECEIVERS = [
+    "http://data_receiver1:8000",
+    "http://data_receiver2:8000",
+]
