4b62eee9280e5278c73a03a16b009e964a77bca5
Yechiel Babani <yechielb2000@gmail.com>
2025-07-09 19:19:09 +0300

    patching gateway

--- cnc_gateway/middlewares/auth_middleware.py
+++ cnc_gateway/middlewares/auth_middleware.py
@@ -6,11 +6,12 @@ from starlette.responses import Response
 
 from redis_client import get_redis
 
+AGENT_HEADER = "X-Agent-ID"
 
 class AuthMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
         redis_client = await get_redis()
-        agent_id = request.headers.get("X-Agent-ID")
+        agent_id = request.headers.get(AGENT_HEADER)
         if not agent_id:
             return Response(status_code=HTTPStatus.NO_CONTENT)
         is_allowed = await redis_client.get(f"agent:{agent_id}")
