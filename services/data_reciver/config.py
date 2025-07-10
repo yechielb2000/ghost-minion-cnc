6f3a9a12a47d5b8b78abac7510c61f120e49b424
Yechiel Babani <yechielb2000@gmail.com>
2025-07-10 14:27:42 +0300

    remove comments

--- services/data_reciver/config.py
+++ services/data_reciver/config.py
@@ -2,7 +2,6 @@ import os
 
 import boto3
 
-# S3 config
 S3_ENDPOINT = os.getenv("S3_ENDPOINT", "https://s3.amazonaws.com")
 S3_ACCESS_KEY = os.getenv("S3_ACCESS_KEY")
 S3_SECRET_KEY = os.getenv("S3_SECRET_KEY")
@@ -10,10 +9,8 @@ S3_REGION = os.getenv("S3_REGION", "us-east-1")
 S3_BUCKET = os.getenv("S3_BUCKET", "my-bucket")
 MAX_FILE_UPLOAD_SIZE = 10 * 1024 * 1024
 
-# Kafka config
 KAFKA_BOOTSTRAP = os.getenv("KAFKA_BOOTSTRAP", "localhost:9092")
 
-# S3 client
 s3_client = boto3.client(
     "s3",
     endpoint_url=S3_ENDPOINT,
