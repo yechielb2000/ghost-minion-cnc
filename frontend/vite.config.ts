00d356b4cd0a8183e3c13c128120ab7a1c9a2b65
Yechiel Babani <yechielb2000@gmail.com>
2025-10-05 20:28:13 +0300

    add assets

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import svgr from "@svgr/rollup";
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    svgr(),
+    react()
+  ],
 })
