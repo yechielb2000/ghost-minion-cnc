787a00d9fe5479bbeae79b585458089f1b89f6a7
Yechiel Babani <yechielb2000@gmail.com>
2025-10-17 12:56:05 +0300

    make AgentTasksPage temaplte

---
+++ frontend/src/pages/AgentTasksPage/index.tsx
@@ -0,0 +1,11 @@
+import type { Agent } from "../../models/Agent"
+
+interface AgentTasksPageProps {
+    agent: Agent
+}
+
+const AgentTasksPage: React.FC<AgentTasksPageProps> = ({ agent }: AgentTasksPageProps) {
+    return (
+        <></>
+    );
+}
